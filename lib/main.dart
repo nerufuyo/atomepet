0f14ac8264ea76f414060b34047f02baf4b88793
nerufuyo <listyoadipamungkas1401@gmail.com>
2025-10-22 19:20:24 +0700

    feat: add main app entry point
    - Configure GetMaterialApp with themes
    - Set up routing and bindings
    - Initialize translations and locale

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get/get.dart';
+import 'package:atomepet/config/themes/app_theme.dart';
+import 'package:atomepet/config/translations/app_translations.dart';
+import 'package:atomepet/routes/app_pages.dart';
+import 'package:atomepet/routes/app_routes.dart';
+import 'package:atomepet/services/database_service.dart';
+import 'package:atomepet/bindings/initial_binding.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await DatabaseService().init();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'AtomePet',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      translations: AppTranslations(),
+      locale: Get.deviceLocale,
+      fallbackLocale: const Locale('en', 'US'),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('en', 'US'), Locale('es', 'ES')],
+      initialBinding: InitialBinding(),
+      initialRoute: AppRoutes.splash,
+      getPages: AppPages.pages,
+    );
+  }
+}
